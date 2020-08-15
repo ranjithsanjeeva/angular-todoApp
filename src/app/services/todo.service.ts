cc55bc4d58a0e773dc6cb8177c59776cf26e088b
Ranjith <ranjithsanjeeva@gmail.com>
2020-08-15 16:24:56 +0530

    My first angular simple todo app

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { Todo } from  '../models/Todo'
+
+const httpOptions = {
+    headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  todoUrls:string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit = '?_limit=5';
+  constructor(private http:HttpClient) { }
+
+  //to get todo
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todoUrls}${this.todosLimit}`)
+  }
+
+  //deleteTodo
+  deleteTodo(todo: Todo):Observable<any> {
+    const url=`${this.todoUrls}/${todo.id}`
+    return this.http.delete<Todo>(url ,httpOptions);
+  }
+
+  //addTodo
+  addTodo(todo: Todo):Observable<any> {
+    return this.http.post<Todo>(this.todoUrls, todo, httpOptions)
+  }
+
+  //Toggle completed
+  toggleCompleted(todo: Todo):Observable<any> {
+    const url=`${this.todoUrls}/${todo.id}`
+    return this.http.put(url, todo, httpOptions);
+  }
+
+}
